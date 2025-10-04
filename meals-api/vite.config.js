4c7594c5c2013f40fd045d81e237b9b1bdc4a7fe
matheustorresdev97 <matheustorresdev@gmail.com>
2025-10-04 18:38:42 -0300

    estrutura inicial

---
+++ meals-api/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    reporters: "verbose",
+
+    pool: "forks",
+    poolOptions: {
+      forks: {
+        singleFork: true,
+      },
+    },
+  },
+});
